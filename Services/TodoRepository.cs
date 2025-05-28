131804c370c582aa5511b327c6e88e9063f20bf3
Zamaychik <mancomddos@gmail.com>
2025-05-28 13:38:55 +0300

    ~

--- Services/TodoRepository.cs
+++ Services/TodoRepository.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
+using TodoManager.Models;
 
-namespace TodoManager.Services
+namespace TodoManager.Services;
+
+public class TodoRepository
 {
-    internal class TodoRepository
+    private readonly string _filePath = Path.Combine(AppContext.BaseDirectory, "todo.json");
+    private List<TodoItem> _items = new();
+
+    public List<TodoItem> GetAll() => _items;
+
+    public void Add(TodoItem item)
     {
+        _items.Add(item);
+    }
+
+    public void Remove(Guid id)
+    {
+        _items.RemoveAll(i => i.Id == id);
+    }
+
+    public TodoItem? FindById(Guid id)
+    {
+        return _items.FirstOrDefault(i => i.Id == id);
+    }
+
+    public async Task SaveAsync()
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        var json = JsonSerializer.Serialize(_items, options);
+        await File.WriteAllTextAsync(_filePath, json);
+    }
+
+    public async Task LoadAsync()
+    {
+        if (!File.Exists(_filePath)) return;
+
+        var json = await File.ReadAllTextAsync(_filePath);
+        var items = JsonSerializer.Deserialize<List<TodoItem>>(json);
+        if (items != null)
+            _items = items;
     }
 }
